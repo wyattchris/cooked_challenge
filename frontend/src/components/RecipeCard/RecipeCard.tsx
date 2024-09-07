617701d1733b68ff1f970ad988f3b43db5efaa9a
lphan48 <laurenphan229@gmail.com>
2024-09-07 12:50:26 -0400

    recipe modal

--- frontend/src/components/RecipeCard/RecipeCard.tsx
+++ frontend/src/components/RecipeCard/RecipeCard.tsx
@@ -46,27 +46,14 @@ export default function RecipeCard({ recipe }: RecipeCardProps) {
           {(onClose) => (
             <>
               <ModalHeader className="flex flex-col gap-1">
-                Modal Title
+                {recipe.name}
               </ModalHeader>
               <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
+                {recipe.meal}
+                <p>Time to Cook</p>
+                {recipe.cook_duration}
+                <p>Instructions</p>
+                {recipe.instructions}
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
