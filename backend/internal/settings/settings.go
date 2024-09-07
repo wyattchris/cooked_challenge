7f5080aeb159a35edcb2ba001a3c93c3e62ee3dd
Stone Liu <liu.sto@northeastern.edu>
2024-09-07 11:55:27 -0400

    Added .env file loading to the server

--- backend/internal/settings/settings.go
+++ backend/internal/settings/settings.go
@@ -1,5 +1,11 @@
 package settings
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 type Settings struct {
 	Application
 	Postgres
@@ -7,5 +13,21 @@ type Settings struct {
 
 // TOOD: implement loading the environment variables
 func Load() (Settings, error) {
-	return Settings{}, nil
+	db_port, err1 := strconv.Atoi(os.Getenv("DB_PORT"))
+	app_port, err2 := strconv.Atoi(os.Getenv("APP_PORT"))
+	if err1 != nil && err2 != nil {
+		fmt.Errorf("No DB_PORT and No App_PORT")
+	}
+	return Settings{
+		Postgres: Postgres{
+			Host:     os.Getenv("DB_HOST"),
+			Name:     os.Getenv("DB_Name"),
+			Port:     db_port,
+			Password: os.Getenv("DB_PASSWORD"),
+			User:     os.Getenv("DB_PASSWORD"),
+		},
+		Application: Application{
+			Port: app_port,
+		},
+	}, nil
 }
