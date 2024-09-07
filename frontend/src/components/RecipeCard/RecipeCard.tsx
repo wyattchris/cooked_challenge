df90c5287ee2942281389c9f669a7028480376b7
lphan48 <laurenphan229@gmail.com>
2024-09-07 12:47:55 -0400

    recipe modal

--- frontend/src/components/RecipeCard/RecipeCard.tsx
+++ frontend/src/components/RecipeCard/RecipeCard.tsx
@@ -1,27 +1,85 @@
 "use client";
 
 import { Recipe } from "@/types/recipe";
-import { Card, CardFooter, Image } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardFooter,
+  Image,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
 
 interface RecipeCardProps {
   recipe: Recipe;
 }
 
 export default function RecipeCard({ recipe }: RecipeCardProps) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
-    <Card>
-      <Image
-        alt="Woman listing to music"
-        className="object-cover"
-        height={200}
-        src={recipe.image_url}
-        width={200}
-      />
-      <CardFooter>
-        <div className="flex justify-between items-center">
-          {recipe.name} {recipe.cook_duration}
-        </div>
-      </CardFooter>
-    </Card>
+    <div>
+      <Card isPressable onPress={onOpen}>
+        <Image
+          alt="Woman listing to music"
+          className="object-cover"
+          height={200}
+          src={recipe.image_url}
+          width={200}
+        />
+        <CardFooter>
+          <div className="flex justify-between items-center">
+            {recipe.name} {recipe.cook_duration}
+          </div>
+        </CardFooter>
+      </Card>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        isDismissable={false}
+        isKeyboardDismissDisabled={true}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Modal Title
+              </ModalHeader>
+              <ModalBody>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Magna exercitation reprehenderit magna aute tempor cupidatat
+                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
+                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
+                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
+                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
+                  eiusmod et. Culpa deserunt nostrud ad veniam.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </div>
   );
 }
